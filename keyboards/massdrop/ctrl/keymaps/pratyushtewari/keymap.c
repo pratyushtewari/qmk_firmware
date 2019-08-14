4a60a88e3472e9be1183b193b047330002b5dbeb
Pratyush Tewari <tewari.pratyush@gmail.com>
2019-08-13 20:55:37 -0400

    Added keyboard led brightness -/+ to F5 and F6

--- keyboards/massdrop/ctrl/keymaps/pratyushtewari/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/pratyushtewari/keymap.c
@@ -32,7 +32,7 @@ keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_BRMD, KC_BRMU, KC_F3,   KC_F4,   BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,            KC_PSCR, KC_SLCK, KC_PAUS, \
+        KC_ESC,  KC_BRMD, KC_BRMU, L_BRD,   L_BRI,   BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,            KC_PSCR, KC_SLCK, KC_PAUS, \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   MO(1),   KC_HOME, KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                                        \
